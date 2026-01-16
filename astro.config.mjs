18f7b3f3cd282114a76b6f18b986ff9db450a958
Carlos Encarnación <carl.enc90@gmail.com>
2026-01-16 07:38:12 -0500

    fix bg-gradient

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+	output: 'server',
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
